a800af061446800d1208bb7c09aba3f3b8da7237
hakuturu583 <ms.kataoka@gmail.com>
2024-06-04 23:30:26 +0900

    modify template
    Signed-off-by: hakuturu583 <ms.kataoka@gmail.com>

--- launch_composition/composition.py
+++ launch_composition/composition.py
@@ -1,8 +1,11 @@
+#!/usr/bin/env python3
+
 from ament_index_python.packages import get_package_prefix, get_package_share_directory
 import xml.etree.ElementTree as ET
 from pathlib import Path
 from typing import List, Dict
 from jinja2 import Template, Environment, FileSystemLoader
+import argparse
 
 
 def get_package_include_directory(package_name: str) -> Path:
@@ -36,7 +39,7 @@ def get_package_launch_directory(package_name: str) -> Path:
     return Path(get_package_share_directory(package_name), "launch")
 
 
-def get_component_containers(tree: ET):
+def get_component_containers(tree: ET, output_file: str):
     assert tree.getroot().tag == "launch"
 
     def get_include_files(tree: ET) -> List[str]:
@@ -66,7 +69,11 @@ def get_component_containers(tree: ET):
             assert child.tag == "composable_node"
             # print(child.attrib)
             ret.append(
-                {"name": child.attrib["name"], "class_name": child.attrib["plugin"]}
+                {
+                    "name": child.attrib["name"],
+                    "namespace": child.attrib["namespace"],
+                    "class_name": child.attrib["plugin"],
+                }
             )
         return ret
 
@@ -84,6 +91,9 @@ def get_component_containers(tree: ET):
             print(
                 "================================ Generated C++ code end. ================================"
             )
+            f = open(output_file, "w")
+            f.write(code)
+            f.close()
 
 
 def generate_cpp_code(
@@ -91,7 +101,14 @@ def generate_cpp_code(
     executor_type: str,
     node_names_and_classnames: List[Dict[str, str]],
 ) -> str:
-    env = Environment(loader=FileSystemLoader("./", encoding="utf8"))
+    env = Environment(
+        loader=FileSystemLoader(
+            Path(get_package_prefix("launch_composition")).joinpath(
+                "lib/launch_composition"
+            ),
+            encoding="utf8",
+        )
+    )
     template = env.get_template("ros2_node_template.jinja")
     return template.render(
         {
@@ -102,15 +119,20 @@ def generate_cpp_code(
     )
 
 
-def composition(package_name: str, launch_filename: str):
+def composition(package_name: str, launch_filename: str, output_file: str):
     launch_filepath = get_package_launch_directory(package_name).joinpath(
         launch_filename
     )
     print("Loading launch file >>> ", launch_filepath)
     tree = ET.parse(launch_filepath)
-    get_component_containers(tree)
+    get_component_containers(tree, output_file)
 
 
 if __name__ == "__main__":
-    composition("perception_bringup", "sensor_bringup.launch.xml")
-    # composition("perception_bringup", "perception_bringup.launch.xml")
+    parser = argparse.ArgumentParser(
+        prog="ros2LaunchComposition", description="Script for generating ROS 2 node."
+    )
+    parser.add_argument("output_file")
+    args = parser.parse_args()
+    # composition("perception_bringup", "sensor_bringup.launch.xml", args.output_file)
+    composition("perception_bringup", "perception_bringup.launch.xml", args.output_file)
